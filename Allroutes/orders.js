82e63e89ffebbc1d2a86cd28f8acd2d0593e7339
liblissz <liblissz3@gmail.com>
2025-08-19 08:59:28 -0700

    nn

--- Allroutes/orders.js
+++ Allroutes/orders.js
@@ -319,10 +319,10 @@ router.get('/receipt/:orderId', async (req, res) => {
  
 
       .font('Helvetica')
-      .text(`Customer Name: `, 500, metaY + 16, { continued: true })
+      .text(`Customer Name: `, 400, metaY + 15, { continued: true })
       .font('Helvetica-Bold').text(order.customer.Name)
        .font('Helvetica')
-      .text(`Address: `, 500, metaY, { continued: true })
+      .text(`Address: `, 400,  metaY + 15, { continued: true })
       .font('Helvetica-Bold').text(order.customer.address);
     // Table header
     const tableTop = 210;
